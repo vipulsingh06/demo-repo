2be9d42871a541c03f34a791ff708c9df56c1cf1
vipulsinghvs05 <vipulsinghvs05@gmail.com>
2026-02-21 16:29:29 +0530

    added java

---
+++ java/Second.java
@@ -0,0 +1,5 @@
+public class Second {
+	public static void main (String[] args){
+		System.out.println("Hello vipul this is your first java file");
+	}
+}
---
+++ java/Third.java
@@ -0,0 +1,7 @@
+public class Third {
+	public static void main(String[] args){
+		System.out.println("Hi your're getting good in this, keep going and oneday u can make all your dreams come true :)");
+		System.out.println("You can learn java also so don't under-estimate yourself vipul you're in the right path");
+	}
+}
+
